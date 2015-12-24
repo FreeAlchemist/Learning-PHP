b45c420ac61a7675a4cb94e6296e28ad11e534b8
FreeAlchemist <freealchemist@mail.ru>
2015-12-24 17:15:27 +0300

    Update exercise.php

--- exercise.php
+++ exercise.php
@@ -9,7 +9,7 @@
 
 //Exercise 01
 echo "<hr>Exercise 01<br>";
-$n=5;
+$n=10;
 $text="Hello World!";
 for ($i = 1; $i <= $n; $i++) {
 
@@ -17,19 +17,43 @@ print($i.": ".$text);
 echo "<br>";
 }
 
+//Exercise 03
+echo "<hr>Exercise 03<br>";
+$n=10;
+$fib = array(1,1);
+print_r($fib);
+echo "<br>";
+for ($i = 2; $i < $n; $i++) {
+	$tmp1 = $fib[$i - 2];
+	$tmp2 = $fib[$i - 1];
+	$fib[$i] = $tmp1+$tmp2;
+}
+print_r($fib);
+
 //Exercise 05
 echo "<hr>Exercise 05<br>";
 
 // $str1="строка для анализа";
 // echo mb_detect_encoding($str1);
-// print($str1);
+// print($str1."<br>");
 
-$str2 = "Two Ts and one F.	Gone for a: walk;
+$str2 = "Two Ts and one F.
+Gone for a: walk ;
+
 today";
-print($str2);
+
+echo("<pre>Two Ts and one F.
+Gone for a: walk;
+today</pre>");
+
+print($str2."<br>");
 
 foreach (count_chars($str2, 1) as $i => $val) {
    echo "\"" , chr($i) , "\" встречается в строке $val раз(а).\n<br>";
 }
+
+//Exercise 06
+echo "<hr>Exercise 06<br>";
+
 
 ?>
