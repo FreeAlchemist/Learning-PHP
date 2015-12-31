175c1dcbed9f3aca52c99b20af40869c4af969db
FreeAlchemist <freealchemist@mail.ru>
2015-12-31 14:55:46 +0300

    Update 10.php

--- 10.php
+++ 10.php
@@ -10,12 +10,17 @@
 $filename="10.xml";
 if (file_exists($filename)) {
     $xml = simplexml_load_file($filename);
- 
     // print_r($xml);
-} else {
+}
+else {
     exit('Failed to open '.$filename);
 }
-echo "<table>";
+echo '<form method="get" action="10form.php">';
+echo '<input type="submit" value="Добавить контакт">';
+echo '<input type="hidden" name="filename" value="'.$filename.'">';
+echo '<input type="hidden" name="add" value="1">';
+echo '</form>';
+echo "<table rules=\"all\">";
 	echo "<tr>
 		<td>ФИО</td>
 		<td>Телефон</td>
@@ -29,7 +34,7 @@ foreach ($xml as $key => $value) {
 		
 	echo "<tr>";
 	echo "<td>";
-	echo "<a href=\"10form.php\"; target=\"_self\" id=".$value['id'].">"
+	echo "<a href=\"10form.php?id=".$value['id']."&filename=".$filename."\">"
 	    .iconv("UTF-8","cp1251",$value->fio->lastname)." "
 	    .iconv("UTF-8","cp1251",$value->fio->firstname)." "
 	    .iconv("UTF-8","cp1251",$value->fio->surname)."</a>";
