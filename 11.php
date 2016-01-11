77fa5650b8238e5111a434f1024be4dd94923af0
FreeAlchemist <freealchemist@mail.ru>
2016-01-11 12:27:23 +0300

    Create 11.php

---
+++ 11.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+echo '<form method="get" action="11_draw.php">';
+echo 'X: '.'<input type="number" size="5" name="getx" required autofocus>'."\r\n";
+echo 'K: '.'<input type="number" size="5" name="getk">'."\r\n";
+echo '<input type="submit" value="Рисовать график y=kx">';
+echo '</form>';
+?>
+</body>
