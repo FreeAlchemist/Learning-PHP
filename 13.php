44272769d6a194f23b6e7e9a22d0ea9ffedc4a38
FreeAlchemist <freealchemist@mail.ru>
2016-01-11 14:50:25 +0300

    Create 13.php

---
+++ 13.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+echo '<form method="get" action="13_show.php">';
+echo 'Dir: '.'<input type="text" size="10" name="dir" required autofocus>'."\r\n";
+echo '<input type="submit" value="Отобразить дерево каталогов">';
+echo '</form>';
+?>
+</body>
