5d6de750db564b325d0f26b6dbe52e19f888b1b7
FreeAlchemist <freealchemist@mail.ru>
2016-01-14 09:22:23 +0300

    Update exercise.php

--- exercise.php
+++ exercise.php
@@ -7,53 +7,126 @@
 </body>
 <?php
 
-//Exercise 01
-echo "<hr>Exercise 01<br>";
-$n=10;
-$text="Hello World!";
-for ($i = 1; $i <= $n; $i++) {
+// echo "<hr>Exercise 01<br>";
+// $n=10;
+// $text="Hello World!";
+// for ($i = 1; $i <= $n; $i++) {
 
-print($i.": ".$text);
-echo "<br>";
-}
+// print($i.": ".$text);
+// echo "<br>";
+// }
 
-//Exercise 03
-echo "<hr>Exercise 03<br>";
-$n=10;
-$fib = array(1,1);
-print_r($fib);
-echo "<br>";
-for ($i = 2; $i < $n; $i++) {
-	$tmp1 = $fib[$i - 2];
-	$tmp2 = $fib[$i - 1];
-	$fib[$i] = $tmp1+$tmp2;
-}
-print_r($fib);
+// echo "<hr>Exercise 02<br>";
+// $n=3;
+// function fact($n){
+// 	if ($n==1) {return 1;}
+// return $n*fact($n-1);
+// }
+// echo fact($n);
 
-//Exercise 05
+// echo "<hr>Exercise 03<br>";
+// $n=10;
+// $fib = array(1,1);
+// print_r($fib);
+// echo "<br>";
+// for ($i = 2; $i < $n; $i++) {
+// 	$tmp1 = $fib[$i - 2];
+// 	$tmp2 = $fib[$i - 1];
+// 	$fib[$i] = $tmp1+$tmp2;
+// }
+// print_r($fib);
+
+// echo "<hr>Exercise 04<br>";
+// class Animal
+// {
+// 	public $food='leaves';
+// 	public function eat (){
+// 		$f="food";
+// 		echo "Eats: ".$this->$f."<hr>";
+// 	}
+
+// 	function __construct() {
+			
+// 	      print "Конструктор класса Animal<br>";
+// 	  }
+// }
+
+// class Mammal extends Animal
+// {
+// 	public $food='milk';
+// 	function __construct() {
+// 	       parent::__construct();
+// 	       print "Конструктор класса Mammal<br>";
+// 	   }
+// }
+
+// class Predator extends Mammal
+// {
+// 	public $food='meat';
+// 	function __construct() {
+// 	       parent::__construct();
+// 	       print "Конструктор класса Predator<br>";
+// 	   }
+// }
+
+// class Cat extends Predator
+// {
+// 	public $food='fish';
+// 	function __construct() {
+// 	       parent::__construct();
+// 	       print "Конструктор класса Cat<br>";
+// 	   }
+// }
+
+// $petr = new Animal();
+// $petr->eat();
+
+// $stepan = new Mammal();
+// $stepan->eat();
+
+// $ivan = new Predator();
+// $ivan->eat();
+
+// $sveta = new CAt();
+// $sveta->eat();
+
 echo "<hr>Exercise 05<br>";
-
-// $str1="строка для анализа";
-// echo mb_detect_encoding($str1);
-// print($str1."<br>");
-
 $str2 = "Two Ts and one F.
-Gone for a: walk ;
-
+Gone 	for a: walk ;
 today";
-
-echo("<pre>Two Ts and one F.
-Gone for a: walk;
-today</pre>");
-
 print($str2."<br>");
 
-foreach (count_chars($str2, 1) as $i => $val) {
-   echo "\"" , chr($i) , "\" встречается в строке $val раз(а).\n<br>";
+$str=str_split($str2);
+print_r($str);
+$strcount = array();
+$wordcount = array();
+$nocount=array('.',',',' ',"\t","\n","\r",';',':');
+foreach ($str as $key => $value) {
+	if (array_search($value, $nocount)===false) {
+		if (array_key_exists($value, $strcount)) {
+				$strcount[$value]++;
+			}
+			else{
+				$strcount[$value]=1;
+			}
+
+		$word .= $value;
+	}
+	else{
+		if (strlen($word)>0) {
+		$wordcount[]=$word;
+		}
+		$word='';
+	}
 }
 
-//Exercise 06
-echo "<hr>Exercise 06<br>";
+echo "<hr>Считаем символы: <br>";
+print_r($strcount);
+
+echo "<hr>Считаем слова: <br>";
+print_r($wordcount);
+print_r(sizeof($wordcount));
+
 
 
 ?>
